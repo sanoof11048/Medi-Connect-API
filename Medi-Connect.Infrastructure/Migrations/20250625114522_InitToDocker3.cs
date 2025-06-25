5a542a9f1b7b838b0e002e52a3e15beb82677cba
Muhammed Sanoof <sanoofmohammed.pvt@gmail.com>
2025-06-25 17:38:03 +0530

    something

---
+++ Medi-Connect.Infrastructure/Migrations/20250625114522_InitToDocker3.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Medi_Connect.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitToDocker3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
